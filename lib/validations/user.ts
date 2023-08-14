03fb05c62faaaaf7af69ed1944efe1fc343ae806
JM_ARAUJO <jmaz.dev@gmail.com>
2023-08-14 16:32:44 -0300

    accProfile(onboarding)/ shadcn/allowConfig/Valid

---
+++ lib/validations/user.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+import React from "react";
+
+const UserValidation = z.object({
+ profile_photo: z.string().url().nonempty(),
+ name: z.string().min(3, { message: "Mínimo 3 caractéres" }).max(30, { message: "Máximo 30 caractéres" }),
+ userName: z.string().min(3, { message: "Mínimo 3 caractéres" }).max(30, { message: "Máximo 30 caractéres" }),
+ bio: z.string().min(3, { message: "Mínimo 3 caractéres" }).max(1000, { message: "Máximo 1000 caractéres" }),
+});
+
+export default UserValidation;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+ experimental: {
+  serverActions: true,
+  serverComponentsExternalPackages: ["mongoose"],
+ },
+ images: {
+  remotePatterns: [
+   {
+    protocol: "https",
+    hostname: "img.clerk.com",
+   },
+   {
+    protocol: "https",
+    hostname: "images.clerk.dev",
+   },
+   {
+    protocol: "https",
+    hostname: "uploadthing.com",
+   },
+   {
+    protocol: "https",
+    hostname: "placehold.co",
+   },
+  ],
+ },
  eslint: {
   // Warning: This allows production builds to successfully complete even if
   // your project has ESLint errors.
