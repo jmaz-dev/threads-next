9dab59eaedd2e801287dadd48f264884d2fc61d7
JM_ARAUJO <jmaz.dev@gmail.com>
2023-08-14 14:18:44 -0300

    global css/layout / routes/ constants navbar

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,21 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from "@clerk/localizations";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import React from "react";
+
+export const metadata = {
+ title: "Welcome - Threads Next",
+ description: "A Next.js 13 Meta Threads Application",
+};
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+ return (
+  <ClerkProvider localization={ptBR}>
+   <html lang="en">
+    <body className={`${inter.className} bg-dark-1`}>{children}</body>
+   </html>
+  </ClerkProvider>
+ );
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,35 @@
+import "../globals.css";
+import { Inter } from "next/font/google";
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from "@clerk/localizations";
+import Topbar from "../components/shared/Topbar";
+import LeftSidebar from "../components/shared/LeftSidebar";
+import RightSidebar from "../components/shared/RightSidebar";
+import Bottombar from "../components/shared/Bottombar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+ title: "Threads Next",
+ description: "A Next.js 13 Meta Threads Application",
+};
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+ return (
+  <ClerkProvider localization={ptBR}>
+   <html lang="en">
+    <body className={inter.className}>
+     <Topbar />
+     <main>
+      <LeftSidebar />
+      <section className="main-container">
+       <div className="w-full max-w-4xl">{children}</div>
+      </section>
+      <RightSidebar />
+     </main>
+     <Bottombar />
+    </body>
+   </html>
+  </ClerkProvider>
+ );
+}
